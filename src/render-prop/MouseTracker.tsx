57d84e064e5e8dddbfac35b869004e9f212dfe75
daikon128 <ndaichi06@gmail.com>
2022-11-03 02:06:51 +0900

    fc+tsでrender prop書いてみた

--- src/render-prop/MouseTracker.tsx
+++ src/render-prop/MouseTracker.tsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, {ReactNode} from "react";
+import logo from '../logo.svg';
 
-type MouseProps = {
-    mouse: {
-        position: {
-            x: number,
-            y: number
-        }
+type MousePositionProps = {
+    position: {
+        x: number,
+        y: number
     }
 }
 
-const Cat: React.FC<MouseProps> = ({mouse}) => {
+const Cat: React.FC<MousePositionProps> = (mouse: MousePositionProps) => {
     return (
-        <img src="/cat.jpg" style={{position: 'absolute', left: mouse.position.x, top: mouse.position.y}}/>
+        // This log is cat, OK??
+        <img src={logo} alt="cat" style={{position: 'absolute', left: mouse.position.x, top: mouse.position.y, width:'20px'}}/>
     );
+}
+
+type MouseProps = {
+    render: (mouse: MousePositionProps) => ReactNode;
+}
+
+const Mouse: React.FC<MouseProps> = ({render}) => {
+    const [positionX, setPositionX] = React.useState(0)
+    const [positionY, setPositionY] = React.useState(0)
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.preventDefault()
+        setPositionX(() => event.clientX)
+        setPositionY(() => event.clientY)
+    }
+
+    return (
+        <div style={{height: '100vh'}} onMouseMove={handleMouseMove}>
+            {
+                render({
+                    position: { x: positionX, y: positionY }
+                })
+            }
+        </div>
+    )
+}
+
+const MouseTracker = () => {
+    return (
+        <div>
+            <h1>move the mouse around</h1>
+            <Mouse render={mouse => (
+                <Cat position={mouse.position} />
+            )} />
+        </div>
+    )
+}
+
+export {
+    MouseTracker
 }
